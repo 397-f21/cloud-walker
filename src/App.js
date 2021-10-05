29c6118fbd98ff34fe5a732d35de3bb448256215
rtl9148 <robertloza2021@u.northwestern.edu>
2021-10-04 20:05:52 -0500

    Added title and bug fixes

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ function App() {
 
   return (
     <div className="App">
+      <h1 className="display-2">CloudWalker</h1>
       <div className="container">
         <div className="row">
-
           <div className="col">
-            <div class="card m-3 p-2">
+            <div className="card m-3 p-2">
               <MapChart setTooltipContent={setContent} location={setLocation} setLocation={setLocation} photos={photos} setPhotos={setPhotos} />
               <ReactTooltip>{content}</ReactTooltip>
             </div>
           </div>
           <div className="col">
-            <div class="card m-3 p-2 mdb-color lighten-2 text-center z-depth-2 scroll">
+            <div className="card m-3 p-2 mdb-color lighten-2 text-center z-depth-2 scroll">
               <h1> Location: {location}</h1>
-              <PhotoCardList photos={photos} />
+              {photos.length === 0 ? "No Photo Available" : <PhotoCardList photos={photos} /> }
             </div>
           </div>
         </div>
