e57e0202b1a1cd7a9ac1cc020ab8d4d12629591e
rgoodman22 <goodman.rob24@gmail.com>
2021-10-04 19:30:08 -0500

    row styling main two cmps

--- src/App.js
+++ src/App.js
@@ -13,16 +13,22 @@ function App() {
 
   return (
     <div className="App">
-      <div class="col-5">
-        <div class="card m-3 p-2 mdb-color lighten-2 text-center z-depth-2">
-          <h1> Location: {location}</h1>
-          <PhotoCardList photos={photos}/>
-        </div>
-      </div>
+      <div className="container">
+        <div className="row">
 
-      <div class="card col-6 m-3 p-2">
-        <MapChart setTooltipContent={setContent} location={setLocation} setLocation={setLocation} photos={photos} setPhotos={setPhotos}/>
-        <ReactTooltip>{content}</ReactTooltip>
+          <div className="col">
+            <div class="card m-3 p-2">
+              <MapChart setTooltipContent={setContent} location={setLocation} setLocation={setLocation} photos={photos} setPhotos={setPhotos} />
+              <ReactTooltip>{content}</ReactTooltip>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card m-3 p-2 mdb-color lighten-2 text-center z-depth-2">
+              <h1> Location: {location}</h1>
+              <PhotoCardList photos={photos} />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
