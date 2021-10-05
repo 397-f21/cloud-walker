f40478123d7cf6bba2a258c716d3b45ccedbee36
QiuOOGan <ganqiu@ucdavis.edu>
2021-10-04 20:36:58 -0500

    alignment on phones

--- src/App.js
+++ src/App.js
@@ -16,13 +16,13 @@ function App() {
       <h1 className="display-2">CloudWalker</h1>
       <div className="container">
         <div className="row">
-          <div className="col">
+          <div className="col-sm-12 col-md-5">
             <div className="card m-3 p-2">
               <MapChart setTooltipContent={setContent} location={setLocation} setLocation={setLocation} photos={photos} setPhotos={setPhotos} />
               <ReactTooltip>{content}</ReactTooltip>
             </div>
           </div>
-          <div className="col">
+          <div className="col-sm-12 col-md-5">
             <div className="card m-3 p-2 mdb-color lighten-2 text-center z-depth-2 scroll">
               <h1> Location: {location}</h1>
               {photos.length === 0 ? "No Photo Available" : <PhotoCardList photos={photos} /> }
